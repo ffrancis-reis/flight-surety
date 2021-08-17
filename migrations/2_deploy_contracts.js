89ad7dba93dd216310dfe74f92faaa76d09ec927
Felipe Reis <ffrancis.reis@gmail.com>
2021-08-17 09:02:13 -0300

    Fixed solidity version, contracts migrations, truffle test gas values

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,23 +1,29 @@
 const FlightSuretyApp = artifacts.require("FlightSuretyApp");
 const FlightSuretyData = artifacts.require("FlightSuretyData");
-const fs = require('fs');
+const fs = require("fs");
 
-module.exports = function(deployer) {
+module.exports = async function (deployer) {
+  const airlineName = web3.utils.utf8ToHex("Condor");
+  //   let firstAirline = "0xf17f52151EbEF6C7334FAD080c5704D77216b732";
 
-    let firstAirline = '0xf17f52151EbEF6C7334FAD080c5704D77216b732';
-    deployer.deploy(FlightSuretyData)
-    .then(() => {
-        return deployer.deploy(FlightSuretyApp)
-                .then(() => {
-                    let config = {
-                        localhost: {
-                            url: 'http://localhost:8545',
-                            dataAddress: FlightSuretyData.address,
-                            appAddress: FlightSuretyApp.address
-                        }
-                    }
-                    fs.writeFileSync(__dirname + '/../src/dapp/config.json',JSON.stringify(config, null, '\t'), 'utf-8');
-                    fs.writeFileSync(__dirname + '/../src/server/config.json',JSON.stringify(config, null, '\t'), 'utf-8');
-                });
-    });
-}
+  await deployer.deploy(FlightSuretyData, airlineName);
+  await deployer.deploy(FlightSuretyApp, FlightSuretyData.address);
+
+  let config = {
+    localhost: {
+      url: "http://localhost:8545",
+      dataAddress: FlightSuretyData.address,
+      appAddress: FlightSuretyApp.address,
+    },
+  };
+  fs.writeFileSync(
+    __dirname + "/../src/dapp/config.json",
+    JSON.stringify(config, null, "\t"),
+    "utf-8"
+  );
+  fs.writeFileSync(
+    __dirname + "/../src/server/config.json",
+    JSON.stringify(config, null, "\t"),
+    "utf-8"
+  );
+};
---
+++ truffle-config.js
@@ -0,0 +1,23 @@
+var HDWalletProvider = require("truffle-hdwallet-provider");
+var mnemonic =
+  "candy maple cake sugar pudding cream honey rich smooth crumble sweet treat";
+
+module.exports = {
+  networks: {
+    development: {
+      // provider: function () {
+      //   return new HDWalletProvider(mnemonic, "http://127.0.0.1:8545/", 0, 50);
+      // },
+      host: "127.0.0.1", // Localhost (default: none)
+      port: 8545, // Standard Ethereum port (default: none)
+      network_id: "*",
+      gas: 6713094,
+      gasLimit: 3141592000000, // Any network (default: none)
+    },
+  },
+  compilers: {
+    solc: {
+      version: "^0.4.24",
+    },
+  },
+};
