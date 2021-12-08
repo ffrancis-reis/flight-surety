65c945f74641a62e66ae9b88f290251b72d46559
ffrancis-reis <ffrancis.reis@gmail.com>
2021-12-08 13:17:01 -0300

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -1,6 +1,5 @@
 var HDWalletProvider = require("truffle-hdwallet-provider");
-var mnemonic =
-  "candy maple cake sugar pudding cream honey rich smooth crumble sweet treat";
+var mnemonic = "";
 
 module.exports = {
   networks: {
